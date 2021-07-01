bc65126acd783278ce0eb07b1cf261eed6701c89
velykitty <jiyunee13@gmail.com>
2021-07-02 01:52:42 +0900

    redux for register, login

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,8 +1,26 @@
 import { createAction, handleActions } from "redux-actions";
 import produce from "immer";
+import {takeLatest} from 'redux-saga/effects';
+import createRequestSaga, {createRequestActionTypes} from '../lib/createRequestSaga';
+import * as authAPI from '../lib/api/auth';
 
 const CHANGE_FIELD = 'auth/CHANGEFIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes(
+    'auth/REGISTER'
+);
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes(
+    'auth/LOGIN'
+);
+
+// const REGISTER = 'auth/REGISTER';
+// const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
+// const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+
+// const LOGIN = 'auth/LOGIN';
+// const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
+// const LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
 
 export const changeField = createAction(
     CHANGE_FIELD,
@@ -18,6 +36,24 @@ export const initializeForm = createAction(
     form => form //register, login
 );
 
+export const register = createAction(REGISTER, ({username, password}) => ({
+    username,
+    password
+}));
+
+export const login = createAction(LOGIN, ({username, password}) => ({
+    username,
+    password
+}));
+
+//사가 생성
+const registerSaga = createRequestSaga(REGISTER, authAPI.register);
+const loginSaga = createRequestSaga(LOGIN, authAPI.login);
+export function* authSaga() {
+    yield takeLatest(REGISTER, registerSaga);
+    yield takeLatest(LOGIN, loginSaga);
+}
+
 const initialState = {
     register: {
         username: '',
@@ -27,7 +63,9 @@ const initialState = {
     login: {
         username: '',
         password: ''
-    }
+    },
+    auth: null,
+    authError: null,
 };
 
 const auth = handleActions(
@@ -35,11 +73,33 @@ const auth = handleActions(
         [CHANGE_FIELD]: (state, {payload: {form, key, value}}) => 
             produce(state, draft => {
                 draft[form][key] = value;
-            }),
+            }
+        ),
         [INITIALIZE_FORM]: (state, {payload: form}) => ({
             ...state,
-            [form]: initialState[form]
-        })     
+            [form]: initialState[form],
+            authError: null, //폼 전호나 시 회원 인증 에러 초기화
+        }),
+        //회원가입 성공
+        [REGISTER_SUCCESS]: (state, {payload: auth}) => ({
+            ...state,
+            authError: null,
+            auth,
+        }),
+        [REGISTER_FAILURE]: (state, {payload: error}) => ({
+            ...state,
+            authError: error,
+        }),
+        //로그인 성공
+        [LOGIN_SUCCESS]: (state, {payload: auth}) => ({
+            ...state,
+            authError: null,
+            auth,
+        }),
+        [LOGIN_FAILURE]: (state, {payload: error}) => ({
+            ...state,
+            authError: error
+        })
     },
     initialState
 );
