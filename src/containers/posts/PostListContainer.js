d8f1f96a3d580989a5c6f4b11214ff1ced26320b
velykitty <jiyunee13@gmail.com>
2021-07-07 15:01:39 +0900

    connect to get post lists API

---
+++ src/containers/posts/PostListContainer.js
@@ -0,0 +1,32 @@
+import React, {useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { withRouter } from "react-router";
+import PostList from "../../components/post/PostList";
+import { listPosts } from "../../modules/posts";
+import qs from 'qs';
+
+const PostListContainer = ({match, location}) => {
+    const dispatch = useDispatch();
+    const {posts, error, loading, user} = useSelector(
+        ({posts, loading, user}) => ({
+            posts: posts.posts,
+            error: posts.error,
+            loading: loading.loading,
+            user: user.user
+        }),
+    );
+
+    useEffect(() => {
+        const {username} = match.params;
+        const {tag, page} = qs.parse(location.search, {
+            ignoreQueryPrefix: true,
+        });
+        dispatch(listPosts({tag, username, page}));
+    }, [dispatch, location.search]);
+
+    return (
+        <PostList loading={loading} error={error} posts={posts} showWriteButton={user} />
+    )
+}
+
+export default withRouter(PostListContainer);
